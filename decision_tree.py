e7c30c70f97f06556dbb5477d3127771c76445e4
ianbarto-cs <treuxxz@icloud.com>
2025-09-10 10:18:11 -0600

    Final?

--- decision_tree.py
+++ decision_tree.py
@@ -9,7 +9,6 @@ sleep(1)
 if decision1 == ("1"):
     snooze()
     decision2 = (input("Would you like to [1] Brush your teeth or [2] Take a shower: "))
-    sleep(1)
     if decision2 == ("1"):
         brush_teeth()
         decision3 = input("Would you like to [1] Go to school or [2] Fake sick and stay home: ")
@@ -25,10 +24,39 @@ if decision1 == ("1"):
     if decision2 == ("2"):
         take_shower()
         decision3 = input("Would you like to get [1] Popcorn or [2] Nerds gummy clusters: ")
-        if decision3 == ("1"):
-            print("Good choice")
+        if decision3 == ("1") or decision3 == ("2"):
+            candy_choice()
+            decision4 = input("You are in your movie and you and your mother are the only people there... Do you [1] get in better seats [2] Keep the seats you payed for: ")
+            if decision4 == ("1"):
+                better_seats()
+            if decision4 == ("2"):
+                keep_seats()
+
 
 if decision1 == ("2"):
     decision2 = input("Congrats on actually waking up early, would you like to [1] Scroll ig reels for 10 minutes or [2] Get breakfast: ")
+    if decision2 == ("1"):
+        ig_reels()
+        decision3 = input("Do you [1] Tell the school that you are not putting your phone in pouch prison, or [2] Comply with the school and do it their way: ")
+        if decision3 == ("1"):
+            no_pouch()
+            decision4 = input("Your time to relax is over, you have 20 minutes till dinner... Do you [1] finish your homeword or [2] Procrastinate till you need to go to bed, and miss out on sleep to do your homework: ")
+            if decision4 == ("1"):
+                finish_homework()
+            if decision4 == ("2"):
+               procrastinate()
+        if decision3 == ("2"):
+            comply_phone()
+    if decision2 == ("2"):
+        get_brecky()
+        decision3 = input("So you finish breakfast and go to school, do you [1] screw around in the middle of class with a substitue, or [2] Focus on your work so you have more freetime later: ")
+        if decision3 == ("1"):
+            sub_mess_around()
+        if decision3 == ("2"):
+            focus_school()
 else:
     print("You typed something wrong, please try again")
+
+print("Thanks for playing the life simulation")
+sleep(0.8)
+print("I hope you made it far")
--- variables.py
+++ variables.py
@@ -58,11 +58,13 @@ def snooze():
     print("You finally need to get out of bed and get your school day started")
 
 def brush_teeth():
+    sleep(1)
     print("Welp, you are still half asleep and drop your toothbrush in the toilet")
     time.sleep(0.5)
     print("Then you remember you still have gum laying on your desk! Problem solving")
 
 def take_shower():
+    sleep(1)
     print("This shower feels like heaven, you sink to Katy Perry and everything")
     time.sleep(0.8)
     print("Even your mom feels the mood and lets you skip school to go to the movies")
@@ -95,3 +97,98 @@ def stay_school():
     print("You all made it out fine...")
     sleep(0.8)
     print("This day could have ended a whole lot worse, instead it was just another boring day at the office")
+
+def no_pouch():
+    print("You get sent to the office, and they are calling your parents")
+    sleep(1)
+    print("Your parents actually agree with you that your phone should not be locked away incase they needed to get in touch with you in the middle of class")
+    sleep(1)
+    print("Your parents sign something saying that they do not want their childs phone to be locked away in a pouch")
+    sleep(1)
+    print("You are now free to go back to class, being the coolest guy in the room with your phone face down on your desk")
+    sleep(1)
+    print("Your school day is finished and you go home and relax for a bit")
+    sleep(1)
+
+def ig_reels():
+    print("Good choice my good sir, but it is going to cost you breakfast")
+    sleep(1)
+    print("It does not matter though, who eats breakfast anyway...")
+    sleep(0.8)
+    print("You are fresh on the ig reels for the moment and show up to school")
+    print("They give you one of those phone prison pouches to make you put your phone away")
+    sleep(1)
+
+def comply_phone():
+    sleep(0.8)
+    print("Since you complied with the phone pouch, your school day goes on as normal")
+    sleep(0.8)
+    print("You need to take the bus back home...")
+    sleep(0.8)
+    print("You hate taking the bus")
+    sleep(0.8)
+    print("You get home, do your homework, than spend the rest of the day at the park with your friends")
+
+def better_seats():
+    sleep(0.8)
+    print("For trying to get better seats, an employee walks in and kicks you out")
+    sleep(0.8)
+    print("You now have to go home, defeated, because you can't enjoy your movie anymore")
+    sleep(0.8)
+    print("You go to bed that night, regretting your decision to switch seats...")
+    sleep(0.8)
+
+def keep_seats():
+    print("You enjoy the rest of your movie, and go out to dinner with your mother")
+    sleep(0.8)
+    print("You get home happy that you got to skip a day of school")
+    sleep(0.8)
+    print("You do your homework and prepare yourself for your next day at school.")
+    sleep(0.8)
+
+def finish_homework():
+    print("Good Boy! You actually decided to do your homework for today")
+    sleep(0.8)
+    print("You finish dinner and go to bed, enjoying 8 hours of beauty sleep")
+    sleep(0.8)
+    print("You wake up the next morning ready to start again")
+    sleep(0.8)
+
+def procrastinate():
+    print("Well you enjoyed your free time before dinner") 
+    sleep(0.8)
+    print("You get done with dinner and fall asleep while doing your homework")
+    sleep(0.8)
+    print("You wake up the next morning panicking, already late to school")
+    sleep(0.8)
+    print("You are cooked")
+    sleep(0.8)
+
+def sub_mess_around():
+    print("You are pushing the limits of the substitute")
+    sleep(0.8)
+    print("She writes your name down while you are watching")
+    sleep(0.8)
+    print("You are cooked buddy")
+    sleep(0.8)
+
+def get_brecky():
+    print("A good breakfast in the morning occasionally could be nice, other than that though, IG reels are the way to go.")
+    sleep(1)
+    print("As long as you aren't Charlie playing clash royale in the morning you are better than the rest of them :)")
+    sleep(0.8)
+
+def focus_school():
+    print("Congrats, you dont have homework")
+    sleep(0.8)
+    print("There is a drawback though, you are now depressed")
+    sleep(0.8)
+    print("Because you actually focused in class, you are being made fun of by the rest of your friends")
+    sleep(0.8)
+    print("You run away never to be seen again")
+
+def candy_choice():
+    print("Good choice")
+    sleep(0.8)
+    print("Your day is pretty easy, enjoy your movie")
+    sleep(0.8)
